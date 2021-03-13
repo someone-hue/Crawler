58382b257df6a4e9523e3d88076cca521d79a51f
Chloris <1373838155@qq.com>
2021-03-13 16:57:01 +0800

    new game

--- 20210313.py
+++ 20210313.py
@@ -1,4 +1,19 @@
 
 import random
-random.randint(1,10)
-print(random.randint(1,10))
+answer = random.randint(1,100)
+counts = 3
+
+while counts > 0:
+    temp = input("猜猜我几岁？")
+    guess = int(temp)
+    
+    if guess == answer:
+        print("猜对啦！")
+        break
+    else:
+        if guess < answer:
+            print("小啦")
+        else:
+            print("大啦")    
+    counts = counts -1
+    
