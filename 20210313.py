1622cec5df7d6430a2bdcefba65aea4ef8e7380b
Chloris <1373838155@qq.com>
2021-03-13 17:05:26 +0800

    add words

--- 20210313.py
+++ 20210313.py
@@ -1,13 +1,11 @@
-
+'''猜猜我的年龄'''
 import random
 answer = random.randint(1,100)
 counts = 3
-
+guess = input("猜猜我几岁？")
 while counts > 0:
-    temp = input("猜猜我几岁？")
-    guess = int(temp)
-    
-    if guess == answer:
+    counts = counts - 1
+    if guess = answer:
         print("猜对啦！")
         break
     else:
@@ -15,5 +13,4 @@ while counts > 0:
             print("小啦")
         else:
             print("大啦")    
-    counts = counts -1
-    
+
