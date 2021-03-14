15326b23c796aed4af8c83444993f68b6d135d38
Chloris <1373838155@qq.com>
2021-03-14 16:14:18 +0800

    add demo1

---
+++ demo1.py
@@ -0,0 +1,20 @@
+'''猜猜我的年龄'''
+import random
+answer = random.randint(1,10)
+counts = 3
+
+while counts > 0:
+    temp = input("猜猜我几岁？")
+    guess = int(temp)
+    if guess == answer:
+        print("猜对啦！")
+        break
+    else:
+        if guess < answer:
+            print("小啦")
+        else:
+            print("大啦")    
+
+    counts = counts - 1
+print("不玩啦")
+print("我的年纪是 %d 岁" %answer)
